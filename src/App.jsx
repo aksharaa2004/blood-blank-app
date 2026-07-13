ed18a7d49a8753a1d9909ac5b27da0d1f723273d
akshara2004 <aksharaa716@gmail.com>
2026-07-13 09:57:26 +0530

    AddDonor added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddDonor from './Components/AddDonor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <AddDonor/>
     </>
   )
 }
